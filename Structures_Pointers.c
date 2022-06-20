ec4962aed685b738adb71b30352f80757a74224a
Ayush <shreyashashank2310@gmail.com>
2022-06-20 08:12:32 +0530

    Structure_Pointers

--- Structures_Pointers.c
+++ Structures_Pointers.c
@@ -10,8 +10,12 @@ int main()
 {
     struct data e1;
     struct data *ptr;
+    struct data *ptr2;
     ptr = &e1;
     (*ptr).code = 101;
+
+    ptr2->code = 102; //you can set the value like this as well
+    printf("The code of the employee is::%d\n", ptr2->code);
     printf("The code of the employee is::%d\n", ptr->code);
     printf("The code of the employee is::%d\n", (*ptr).code);
     printf("The code of the employee is::%d\n", e1.code);
